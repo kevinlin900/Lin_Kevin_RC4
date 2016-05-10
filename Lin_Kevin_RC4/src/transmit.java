290c2db3377b6679daf2dfba687a085ca60611eb
marieshi <marieshi99@gmail.com>
2016-05-10 14:57:54 -0700

    Update transmit.java

--- Lin_Kevin_RC4/src/transmit.java
+++ Lin_Kevin_RC4/src/transmit.java
@@ -1,6 +1,8 @@
 
 public class transmit implements RC4Definition {
 <<<<<<< HEAD
+	private int S[] = new int[256];
+	private int K[] = new int [256];
 	public static void main(String[] args) {
 
 	}
@@ -9,20 +11,25 @@ public class transmit implements RC4Definition {
 
 	}
 
-	public void KSA() {
-
+	public byte [] encrypt(int[] plaintext){
+	int j =0;
+	int temp = 0;
+	for (int i = 0; i<255; i++){
+		S[i] = i;
+		K[i] = key[i % key.length];
+		j = j+S[i] + K[i%1];
+		temp = S[i];
+		S[i] = S[j];
+		S[j] = temp;
 	}
 
 	public void PRGA() {
-
+	
 	}
 
 	public void PrintResult() {
 
 =======
-  private int S[] = new int[256];
-	public String [] encrypt(int[] plaintext){
-	  
->>>>>>> origin/master
+
 	}
 }
